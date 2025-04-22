f19110dac17c6bfac1907c18ec80b636d738b74b
Albert <albertherrero29@gmail.com>
2025-04-22 16:51:38 +0200

    Api Routes (fixed class names)

--- routes/api.php
+++ routes/api.php
@@ -17,39 +17,39 @@ Route::get('/users' , function () {
 
 
 //// COMPETITIONS
-Route::get('/competition/list', [CompetitionController::class, 'list'])->name('competition_list');
-Route::match(['get', 'post'], '/competition/new', [CompetitionController::class, 'new'])->name('competition_new');
-Route::match(['get', 'post'], '/competition/edit/{id}', [CompetitionController::class, 'edit'])->name('competition_edit');
+Route::get('/competition/list', [CompetitionController::class, 'show'])->name('competition_list');
+Route::match(['get', 'post'], '/competition/new', [CompetitionController::class, 'store'])->name('competition_new');
+Route::match(['get', 'post'], '/competition/edit/{id}', [CompetitionController::class, 'update'])->name('competition_edit');
 Route::get('/competition/delete/{id}', [CompetitionController::class, 'delete'])->name('competition_delete');
 
 //// RACES
-Route::get('/race/list', [RaceController::class, 'list'])->name('race_list');
-Route::match(['get', 'post'], '/race/new', [RaceController::class, 'new'])->name('race_new');
-Route::match(['get', 'post'], '/race/edit/{id}', [RaceController::class, 'edit'])->name('race_edit');
+Route::get('/race/list', [RaceController::class, 'show'])->name('race_list');
+Route::match(['get', 'post'], '/race/new', [RaceController::class, 'store'])->name('race_new');
+Route::match(['get', 'post'], '/race/edit/{id}', [RaceController::class, 'update'])->name('race_edit');
 Route::get('/race/delete/{id}', [RaceController::class, 'delete'])->name('race_delete');
 
 //// TRACKS
-Route::get('/track/list', [TrackController::class, 'list'])->name('track_list');
-Route::match(['get', 'post'], '/track/new', [TrackController::class, 'new'])->name('track_new');
-Route::match(['get', 'post'], '/track/edit/{id}', [TrackController::class, 'edit'])->name('track_edit');
+Route::get('/track/list', [TrackController::class, 'show'])->name('track_list');
+Route::match(['get', 'post'], '/track/new', [TrackController::class, 'store'])->name('track_new');
+Route::match(['get', 'post'], '/track/edit/{id}', [TrackController::class, 'update'])->name('track_edit');
 Route::get('/track/delete/{id}', [TrackController::class, 'delete'])->name('track_delete');
 
 //// TEAMS
-Route::get('/team/list', [TeamController::class, 'list'])->name('team_list');
-Route::match(['get', 'post'], '/team/new', [TeamController::class, 'new'])->name('team_new');
-Route::match(['get', 'post'], '/team/edit/{id}', [TeamController::class, 'edit'])->name('team_edit');
+Route::get('/team/list', [TeamController::class, 'show'])->name('team_list');
+Route::match(['get', 'post'], '/team/new', [TeamController::class, 'store'])->name('team_new');
+Route::match(['get', 'post'], '/team/edit/{id}', [TeamController::class, 'update'])->name('team_edit');
 Route::get('/team/delete/{id}', [TeamController::class, 'delete'])->name('team_delete');
 
 //// DRIVERS
-Route::get('/driver/list', [DriverController::class, 'list'])->name('driver_list');
-Route::match(['get', 'post'], '/driver/new', [DriverController::class, 'new'])->name('driver_new');
-Route::match(['get', 'post'], '/driver/edit/{id}', [DriverController::class, 'edit'])->name('driver_edit');
+Route::get('/driver/list', [DriverController::class, 'show'])->name('driver_list');
+Route::match(['get', 'post'], '/driver/new', [DriverController::class, 'store'])->name('driver_new');
+Route::match(['get', 'post'], '/driver/edit/{id}', [DriverController::class, 'update'])->name('driver_edit');
 Route::get('/driver/delete/{id}', [DriverController::class, 'delete'])->name('driver_delete');
 
 //// RANKINGS
-Route::get('/ranking/list', [RankingController::class, 'list'])->name('ranking_list');
-Route::match(['get', 'post'], '/ranking/new', [RankingController::class, 'new'])->name('ranking_new');
-Route::match(['get', 'post'], '/ranking/edit/{id}', [RankingController::class, 'edit'])->name('ranking_edit');
+Route::get('/ranking/list', [RankingController::class, 'show'])->name('ranking_list');
+Route::match(['get', 'post'], '/ranking/new', [RankingController::class, 'store'])->name('ranking_new');
+Route::match(['get', 'post'], '/ranking/edit/{id}', [RankingController::class, 'update'])->name('ranking_edit');
 Route::get('/ranking/delete/{id}', [RankingController::class, 'delete'])->name('ranking_delete');
 
 Route::get('/saludo', function () {
