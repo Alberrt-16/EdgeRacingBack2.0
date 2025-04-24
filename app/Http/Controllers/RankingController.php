1c88aab392d2681375dc82e27b4704e76cc29450
Albert <albertherrero29@gmail.com>
2025-04-24 17:39:02 +0200

    show all rankings unordered

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class RankingController extends Controller
 {
     public function index() {
-        return Ranking::groupBy('race_id');
+        return Ranking::all();
     }
 
     public function store(Request $request) {
